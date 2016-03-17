2a8057a14b68ad7c33fab9f631699259927d7492
1102137242 <ww0986360075@gmail.com>
2016-03-17 08:07:59 +0800

    test

---
+++ eSale/Controllers/OrderController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eSale.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustomerID = order.CustomerID;
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder()
+        {
+           
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的ACTION
+        /// </summary>
+        /// <param name="Order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order Order)
+        {
+
+            return View("Index");
+        }
+    }
+}
