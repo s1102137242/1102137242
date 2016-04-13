0a6dcc3b5e0ae19c6802d2d5ea10e781f3897d3e
JasonLo <ww0986360075@gmail.com>
2016-04-13 23:30:53 +0800

    select finish

---
+++ eSale/Controllers/EmployeesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using eSale.Models;
+
+namespace eSale.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(Models.SelectSearch selectitem)
+        {
+            EmployeesService EmployeesService = new EmployeesService();
+            List<Employees> result = EmployeesService.GetEmployeesName();
+
+            List<SelectListItem> EmployeesData = new List<SelectListItem>();
+            EmployeesData.Add(new SelectListItem()
+            {
+                Text = "",
+                Value = null
+            });
+            foreach (var item in result)
+            {
+                EmployeesData.Add(new SelectListItem()
+                {
+                    Text = item.LastName,
+                    Value = item.EmployeeID.ToString()
+                });
+                ViewData["EmpData"] = EmployeesData;
+            }
+
+            ShippersService ShippersService = new ShippersService();
+            List<Shippers> result1 = ShippersService.GetShippersName();
+
+            List<SelectListItem> ShippersData = new List<SelectListItem>();
+            ShippersData.Add(new SelectListItem()
+            {
+                Text = "",
+                Value = null
+            });
+            foreach (var item in result1)
+            {
+                ShippersData.Add(new SelectListItem()
+                {
+                    Text = item.CompanyName,
+                    Value = item.ShipperID.ToString()
+                });
+                ViewData["ShippersData"] = ShippersData;
+            }
+
+
+            OrdersService OrdersService = new OrdersService();
+            ViewBag.SelectData = OrdersService.SelectOrder(selectitem);
+
+
+            return View();
+
+        }
+
+        [HttpPost()]
+        public JsonResult DeleteOrder(string OrderID)
+        {
+
+            try
+            {
+                OrdersService OrdersService = new OrdersService();
+                OrdersService.DeleteOrderByID(OrderID);
+                return this.Json(true);
+            }
+            catch (Exception)
+            {
+                return this.Json(false);
+            }
+        }
+    }
+}
